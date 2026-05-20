d418e86f8a3118beaee1a8ee4cc3c7bfa1f7ab61
Deathryuk20 <deathryuk20@gmail.com>
2026-05-20 21:24:43 +0530

    fic 11

--- src/main.jsx
+++ src/main.jsx
@@ -11,13 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
